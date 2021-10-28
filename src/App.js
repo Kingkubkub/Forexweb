7a693aac57c2faf5a62cd56287339a363ebfc07f
Kingkubkub <king33130h@gmail.com>
2021-10-29 01:10:19 +0700

    Rout

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Index from './Page/Index';
 import Home from './Page/Home';
 import Setting from './Page/Setting';
 import Forex from './Page/Forex';
-import { BrowserRouter, Route, Router } from 'react-router-dom'
+import { BrowserRouter, Route, Router,HashRouter } from 'react-router-dom'
 import { ThemeProvider } from '@mui/material';
 import { createTheme } from '@mui/material/styles';
 import Forgotpssword from './Page/Forgotpssword';
@@ -17,13 +17,13 @@ function App() {
 
 
     <div>
-      <BrowserRouter>
+      <HashRouter>
         <Route exact path="/" component={Index} />
         <Route path="/home" component={Home} />
         <Route path="/setting" component={Setting} />
         <Route path="/forex" component={Forex} />
         <Route path="/Forgotpssword" component={Forgotpssword} />
-      </BrowserRouter>
+      </HashRouter>
     </div>
 
   );
