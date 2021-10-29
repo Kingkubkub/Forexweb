695c2847d465f06df1db9e3c83497fb5da98d43c
Kingkubkub <king33130h@gmail.com>
2021-10-29 13:46:24 +0700

    Rapon

--- src/Page/Buttonmebu.js
+++ src/Page/Buttonmebu.js
@@ -9,18 +9,18 @@ function Buttonmenu() {
         <div style={{ backgroundColor: "#f8f7f6", height: "100px", padding: "1.5%",color:"#111111" }}>
             <Container  >
                 <Grid container >
-                    <Grid item lg={6}>
+                    <Grid item  lg={6} xs={6}>
                         <Typography variant="h7" style={{ fontWeight: "bolder" }}>Green Graph</Typography>
 
                     </Grid>
-                    <Grid item lg={2}>
+                    <Grid item lg={2} xs={6}>
                         <Typography variant="h7">เกี่ยวกับเรา</Typography>
                     </Grid>
-                    <Grid item lg={2}>
+                    <Grid item lg={2} xs={6}>
                         <Typography variant="h7">ช่องทางการติดต่อ</Typography>
                     </Grid>
 
-                    <Grid item lg={2}>
+                    <Grid item lg={2} xs={6}>
                         <Typography variant="h7">ข้อกำหนดการใช้งาน</Typography>
                     </Grid>
                 </Grid>
