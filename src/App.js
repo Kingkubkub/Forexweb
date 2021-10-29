9e8ff7f960adf0c68d498d2c6f238ebb40e416d2
Kingkubkub <king33130h@gmail.com>
2021-10-29 19:39:49 +0700

    Ront

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Index from './Page/Index';
 import Home from './Page/Home';
 import Setting from './Page/Setting';
 import Forex from './Page/Forex';
-import { BrowserRouter, Route, Router, HashRouter } from 'react-router-dom'
+import { BrowserRouter, Route, Router, HashRouter,Switch } from 'react-router-dom'
 import { ThemeProvider } from '@mui/material';
 import { createTheme } from '@mui/material/styles';
 import Forgotpssword from './Page/Forgotpssword';
@@ -17,13 +17,13 @@ function App() {
 
 
     
-      <HashRouter basename="/Forexweb">
-        <Route exact path='/Forexweb' component={Index} />
+      <Switch>
+        <Route exact path='/Forexweb/' component={Index} />
         <Route path='/Forexweb/home' component={Home} />
         <Route path='/Forexweb/setting' component={Setting} />
         <Route path='/Forexweb/forex' component={Forex} />
         <Route path='/Forexweb/Forgotpssword' component={Forgotpssword} />
-      </HashRouter>
+      </Switch>
    
 
   );
